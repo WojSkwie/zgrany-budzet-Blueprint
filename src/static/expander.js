07708d93b2a84f5f3a8372f546ae19372f215ec0
Artur Pawelczyk <pchairman@gmail.com>
2025-12-06 22:24:48 +0100

    \#24 Dashboard expandable lists

---
+++ src/static/expander.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function () {
+    function toggle(event) {
+        let rowId = 'details-' + event.target.getAttribute('office-name');
+        let detailsRow = document.getElementById(rowId);
+
+        if (!detailsRow) {
+            return;
+        }
+
+        if (detailsRow.style.display === 'none') {
+            detailsRow.style.display = 'table-row';
+            event.target.innerHTML = 'Ukryj szczegóły';
+        } else {
+            event.target.innerHTML = 'Pokaż sczegóły';
+            detailsRow.style.display = 'none';
+        }
+
+    }
+
+    document.querySelectorAll('.show-office-details').forEach((btn) => btn.addEventListener('click', toggle));
+});
